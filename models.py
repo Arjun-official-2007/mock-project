cd24aa306bbf8c8e73a3e6adbcf5ed0c8f605278
Darshan <midoria646@gmail.com>
2026-02-08 22:32:27 +0530

    Update pydantic models

--- models.py
+++ models.py
@@ -5,9 +5,8 @@ class TradeRequest(BaseModel):
     symbol: str          
     action: str          
     quantity: int        
-    price_limit: Optional[float] = None 
+
 
 class Asset(BaseModel):
     symbol: str
     current_value: float
-    volatility: str
